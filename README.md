693332b38dd152f42541422c35fb845e0500bb2b
Josh Rogan <jjjr1122@gmail.com>
2016-02-05 22:52:34 -0500

    Add readme. Add mostFrequent function.

---
+++ README.md
@@ -0,0 +1,4 @@
+# JS ES6 Solutions to CS Questions 
+
+Solutions in ES6 to common interview questsions for CS positions. 
+
---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * Return the most frequent item in the given array 
+ * @param  {Array} array Input array 
+ * @return The value most repeated in the array 
+ */
+function mostFrequent(array){
+  let currentBestIndex = -1;
+  let maxValue = 0; 
+  let lookup = [];
+  
+	array.map(x => {
+    if(lookup[x] === undefined){
+      lookup[x] = 1; 
+    }
+    else{
+    	lookup[x]++;
+    }
+    
+     if(lookup[x] > maxValue){
+          currentBestIndex = x;
+          maxValue = lookup[x];
+     }
+
+  });
+  return currentBestIndex; 
+}
+
+/**
+ * Find all pairs of integers whose sum is ten
+ * @param  {[type]} array [description]
+ * @return        [description]
+ */
+function pairsSum10(array){
+
+}
+
+
+
