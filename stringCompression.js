d43a3f793ca0e428ac73f37702f5902df43a40d4
Josh Rogan <jjjr1122@gmail.com>
2016-02-20 15:39:47 -0500

    Add string compression task

---
+++ stringCompression.js
@@ -0,0 +1,69 @@
+/**
+ * @fileoverview Use a very simple compression converting sequences of same characters to 
+ * the number of occurences. 
+ *     Ex:  aaaaabbbbbccddde = a5b5c2d3e
+ *     
+ *     
+ * @author JoshJRogan@gmail.com (Josh Rogan)
+ */
+
+'use strict';
+
+/**
+ * 
+ *
+ * 
+ * @param  {Array} array Input array 
+ * @return The value most repeated in the array 
+ */
+
+/**
+ * Return a either a shorter compressed string (a2b3b2a1) or the original string
+ *
+ * Runtime: O(n) 
+ * @param  {String} string any input string
+ * @return {String}        shorter compressed or original string 
+ */
+function compressStringFirst(string){
+    //contains an array of objects of {char: 'c', count: 4}
+    let stringCountsArray = []; 
+
+
+    let currentCount = 1; 
+    let lastChar = false; 
+
+    for(let char of string.split('')){
+        //New char found
+        if(lastChar !== char){
+            //Add last char to string counts obj
+            if(lastChar !== false){
+                stringCountsArray.push({char: lastChar, count: currentCount}); 
+            }
+            currentCount = 1;
+            lastChar = char;
+        }
+        else{
+            currentCount++;
+        }
+    }
+
+    //Create a compressed string
+    let compressedString = createStringFromCountArray(stringCountsArray);
+
+    //Only return the shorter string
+    return (compressedString.length < string.length) ? compressedString : string;
+}
+
+
+//Helper function to create the string from the string counts objects
+function createStringFromCountArray(stringCountsArray){
+    let outputString = ''; 
+    for(let countObj of stringCountsArray){
+        outputString += countObj.char + countObj.count; 
+    }
+    return outputString; 
+}
+
+
+console.log(compressStringFirst('aaAAAabbcdefff'));
+console.log(compressStringFirst('aaAAAabbcdedddddddassdfasdsssssssssssssssfff'));
