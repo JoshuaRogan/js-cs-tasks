e653266f94a5345060d781557b43f7c193830017
Josh Rogan <jjjr1122@gmail.com>
2016-02-20 17:17:45 -0500

    Add naive contains

---
+++ stringSubstring.js
@@ -0,0 +1,67 @@
+/**
+ * @fileoverview Determine if a string is a substring of another  
+ *    
+ *     
+ *     
+ * @author JoshJRogan@gmail.com (Josh Rogan)
+ */
+
+'use strict';
+
+//Exists in es6 so safe to add to prototype
+String.prototype.contains = naiveContains;
+
+
+//Naive brute force approach = O(n^2)
+function naiveContains(compareTo){
+    //Quick Checks
+    if(this.length < compareTo.length) return false; 
+    if(this.length === compareTo.length){
+        if(this === compareTo) return true; 
+        else return false; 
+    } 
+
+    let iterations = 0; 
+
+    for(let i=0; i < this.length; i++){
+        let tempI = i;
+        for(let j=0; j < compareTo.length; j++){
+            iterations++;
+            console.log('comparing', this.charAt(tempI), compareTo.charAt(j));
+            if(this.charAt(tempI) === compareTo.charAt(j)){
+                tempI++;
+                //At the end of the comparision string
+                if(j === compareTo.length - 1){
+                    console.log(iterations);
+                    return true;
+                }
+            }
+            else{ //end interior loop
+                j = compareTo.length; 
+            }
+        }
+    }
+
+    console.log(iterations);
+    return false;
+}
+
+
+//Make it faster 
+function smarterContains(compareTo){
+    function traverse(){
+
+    }
+}
+
+console.log('123456789'.contains('6789'));
+
+
+// console.log('ccat'.contains('cat'));
+// console.log('ccat'.contains('cc'));
+// console.log('ccat'.contains('nope'));
+// console.log('ccat'.contains('nopecat'));
+// console.log('nopecat'.contains('cat'));
+
+
+// console.log('cat'.contains('cat'));
